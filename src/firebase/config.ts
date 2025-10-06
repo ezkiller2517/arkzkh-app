f67f9b7b619ddcbc62e7fa84745017899b37710d
Ezekiel Love <EzekielZechariahLove@gmail.com>
2025-10-06 19:15:33 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2410503780-807e6",
+  "appId": "1:63654890665:web:005d02deafef3495906db8",
+  "apiKey": "AIzaSyB8jRiL3Pm-cWprObSJdTHBRZ4R9zwU01k",
+  "authDomain": "studio-2410503780-807e6.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "63654890665"
+};
